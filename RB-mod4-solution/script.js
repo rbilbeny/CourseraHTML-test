d83272440541d691ae1a72e03e1d4691e5d49c86
rbilbeny <105988537+rbilbeny@users.noreply.github.com>
2022-06-30 00:30:58 -0400

    fist attempt of the module 4 assignment solution

--- RB-mod4-solution/script.js
+++ RB-mod4-solution/script.js
@@ -34,14 +34,16 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
+(function (window) {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using either the helloSpeaker's or byeSpeaker's 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+var firstLetter = null;
+for (var i = 0; i < names.length ; i++) {
+firstLetter = names[i].charAt(0);
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -56,9 +58,11 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if ((firstLetter == "j") || (firstLetter == "J")) {
+    byeSpeaker.speak (names[i]);
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak (names[i]);
   }
-}
+}  
+})(window);
+
